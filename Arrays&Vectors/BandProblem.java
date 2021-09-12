2644f430f5b9e188384360c67d3b135b74c69cd7
HimaniPanwar <himanipanwar34@gmail.com>
2021-09-12 12:18:26 +0530

    First Commit - Arrays & Vectors

---
+++ Arrays&Vectors/BandProblem.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class BandProblem{
+    
+    public static int largestBand(int[] arr){
+
+        Set<Integer> set = new HashSet<Integer>();
+
+        int longestBand = 1;
+
+        for(int it: arr){
+            set.add(it);
+        }
+
+        for(int p : arr){
+
+            int parent = p-1;
+
+            if(!set.contains(parent)){
+                int next_num = p + 1;
+                int cnt = 1;
+
+                while(set.contains(next_num)){
+                    next_num++;
+                    cnt++;
+                }
+
+                longestBand = Math.max(longestBand,cnt);
+            }
+            
+        }
+
+        return longestBand;
+    } 
+
+    public static void main(String[] args){
+        int[] arr = {1,9,3,0,18,5,2,4,10,7,12,6};
+
+        System.out.println(largestBand(arr));
+    }
+}
---
+++ Arrays&Vectors/ThreeSum.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class ThreeSum {
+
+        //Function to return triplets whose sum equals to zero (No duplicates)
+        public List<List<Integer>> threeSum(int[] nums) {
+            List<List<Integer>> result = new ArrayList<List<Integer>>();
+            
+            Set<List<Integer>> set = new HashSet<List<Integer>>();
+            
+            //Sorting an array
+            Arrays.sort(nums);
+            
+            int n = nums.length;
+            
+            for(int i=0;i<n-2;i++){
+                //Two pointer approach
+                int j = i+1;
+                int k = n-1;
+               
+                int target = 0 - nums[i];
+                
+                while(j<k){
+                    
+                    int curr_sum = nums[j];
+                    curr_sum += nums[k];
+                          
+                    if(curr_sum == target){
+                        set.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                        j++;
+                        k--;
+                    }else if(curr_sum > target){
+                        k--;
+                    }else{
+                        j++;
+                    }
+                    
+                    
+                }
+            }
+            
+            for(List<Integer> t: set){
+                result.add(t);
+            }
+            
+            return result;
+        }
+}
+
+// Link to Leetcode Problem -> https://leetcode.com/problems/3sum/ 
