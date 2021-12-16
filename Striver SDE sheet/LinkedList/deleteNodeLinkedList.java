ec5cfa83d4ea7289babeb25da2e010f3c6d7580e
HimaniPanwar <himanipanwar34@gmail.com>
2021-12-17 03:04:02 +0530

    LinkedList programs

---
+++ Striver SDE sheet/LinkedList/deleteNodeLinkedList.java
@@ -0,0 +1,9 @@
+
+public class deleteNodeLinkedList {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+   }
+}
+
+//Problem Link : https://leetcode.com/problems/delete-node-in-a-linked-list/
