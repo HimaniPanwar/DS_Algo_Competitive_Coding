acd1b6b83960ffaa69857ce5c7904ec162fc30c7
HimaniPanwar <himanipanwar34@gmail.com>
2021-12-23 02:35:34 +0530

    Check starting point of cycle in linked list

---
+++ StriverSDEsheet/LinkedList/headOfCycle.java
@@ -0,0 +1,31 @@
+package StriverSDEsheet.LinkedList;
+
+public class headOfCycle {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null)
+            return null;
+        
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode entry = head;
+        
+        while(fast.next!=null && fast.next.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            
+            if(fast==slow){
+                while(slow!=entry){
+                    slow = slow.next;
+                    entry = entry.next;
+                }
+                return entry;
+            }
+                
+        }
+        
+        return null;
+        
+    }
+}
+
+//Problem Link: https://leetcode.com/problems/linked-list-cycle-ii/
